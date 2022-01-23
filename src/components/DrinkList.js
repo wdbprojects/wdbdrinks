57f2504d31ca8c4e5ae19d8ea74507e82012676a
Ronaldo Ortiz <ronyortizop@gmail.com>
2022-01-22 22:40:09 -0400

    test continous deployment

--- src/components/DrinkList.js
+++ src/components/DrinkList.js
@@ -9,15 +9,11 @@ const DrinkList = () => {
     return <Loading />;
   }
   if (myDrinks.length < 1) {
-    return (
-      <h2 className="section-title">
-        no drinks match your criteria
-      </h2>
-    );
+    return <h2 className="section-title">no drinks match your criteria</h2>;
   }
   return (
     <section className="section">
-      <h2 className="section-title">my drinks</h2>
+      <h2 className="section-title">my cool drinks</h2>
       <div className="cocktails-center">
         {myDrinks.map((item) => {
           return <Drink key={item.id} {...item} />;
